c5ec6b729a8b8db8814564b3f8cc24392ebab228
Ted Pudlik <tpudlik@gmail.com>
2016-01-07 22:08:55 -0500

    Implmented mpmath tests of hyp1f1.
    The tests are based on http://arxiv.org/abs/1407.7786.
    
    At the moment, the tests for real inputs fail, while those
    for complex inputs throw a TypeError (since hyp1f1 does not
    support complex a, b parameters).

---
+++ scipy/special/hypergeometric.py
@@ -0,0 +1,7 @@
+"""
+An implementation of the confluent hypergeometric function based on
+Pearson, Olver, and Porter, "Numerical Methods for the Computation
+of the Confluent and Gauss Hypergeometric Functions",
+http://arxiv.org/abs/1407.7786.
+
+"""
